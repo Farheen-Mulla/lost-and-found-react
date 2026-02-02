599702e97ba46ba870f21179dbf61a50164a5836
Farheen-Mulla <mullaf909@gmail.com>
2026-02-02 13:45:16 +0530

    add form component

--- lost-found-react/src/components/itemform.jsx
+++ lost-found-react/src/components/itemform.jsx
@@ -1,17 +1,31 @@
+import {useState} from 'react';
 function ItemForm(){
+     const [name , setName] = useState("");
+     const [desc , setDesc] = useState("");
+     const [Contact , setContact] = useState("");
+     const [status , setStatus] = useState("lost");
+     const [img , setImg] = useState("null");
+
+     function handleSubmit(e) {
+       e.preventDefault();
+       const itemData = {
+        name,desc,Contact,status,img
+       };
+       console.log(itemData)
+     }
     return(
-        <div className="itemForm">
+        <form className="itemForm" onSubmit={handleSubmit}>
             <h2>Fill Item Information :</h2>
-            <input type="text" placeholder="Item Name" required/><br/>
-            <input type="text" placeholder="Item Description" required/><br/>
-            <input type="text" placeholder="Contact Information" required/><br/>
-            <input type="file" accept="image/*" /><br/>
-            <select>
+            <input type="text" placeholder="Item Name"  value={name} onChange={(e) => setName(e.target.value)} required/><br/>
+            <input type="text" placeholder="Item Description" value={desc} onChange={(e) => setDesc(e.target.value)} required/><br/>
+            <input type="text" placeholder="Contact Information"value={Contact} onChange={(e) => setContact(e.target.value)}  required/><br/>
+            <input type="file" accept="image/*"  onChange={(e) => setImg(e.target.files[0])}/><br/>
+            <select value={status} onChange={(e.target.value)}>
                 <option value="lost">Lost</option>
                 <option value="found">Found</option>
             </select><br/>
             <button type="submit">Add Item</button>
-        </div>
+        </form>
     );
 }
 

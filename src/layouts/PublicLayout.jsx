2cb3b7799a769c3d6178719ba53097d43f9313c8
Farheen-Mulla <mullaf909@gmail.com>
2026-02-12 20:29:34 +0530

    correcting file names for deployment

--- src/layouts/PublicLayout.jsx
+++ src/layouts/PublicLayout.jsx
@@ -1,5 +1,5 @@
 import PublicHeader from "../components/PublicHeader";
-import Footer from "../components/Footer";
+import Footer from "../components/footer";
 
 export default function PublicLayout({children}){
     return(

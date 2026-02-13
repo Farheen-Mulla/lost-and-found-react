432938156b10b764a20688eee05c265a8f5a3f00
Farheen-Mulla <mullaf909@gmail.com>
2026-02-13 07:33:28 +0530

    Fix navbar links

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -14,8 +14,13 @@ function Header({searchQuery , onSearchChange , searchStatus , onStatusChange , 
                 <SearchBar searchQuery={searchQuery} onSearchChange={onSearchChange} searchStatus={searchStatus} onStatusChange={onStatusChange} />
             </div>
             <div className="flex space-x-6 text-lg font-medium mr-20 border-l-2 border-gray-300 pl-6">
-                <Link to="/">Home</Link>
-                <Link to="/submit">Submit</Link>
+                {location.pathname !== "/submit" && (
+        <Link className="ml-4 text-lg font-medium px-4 py-2 rounded-lg transition-all duration-200 hover:bg-white hover:text-[#1a3a8a] hover:shadow-md" to="/submit">Submit</Link>
+      )}
+      
+      {location.pathname !== "/items" && (
+        <Link className="ml-4 text-lg font-medium px-4 py-2 rounded-lg transition-all duration-200 hover:bg-white hover:text-[#1a3a8a] hover:shadow-md" to="/items">Items</Link>
+      )}
                 {isLoggedIn ? (<button onClick={handleLogoutClick} className="bg-red-500 px-4 py-1 rounded-lg hover:bg-red-600 transition-colors">Logout</button>) : (<Link to="/login">Login</Link>)}
             </div>
         </div>

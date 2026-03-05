6e34d34bccbd4136d4bad675e8ae1467922439f1
Farheen-Mulla <mullaf909@gmail.com>
2026-03-05 19:28:54 +0530

    chore: prepare backend for Render deployment

--- lost-found-backend/src/server.js
+++ lost-found-backend/src/server.js
@@ -7,6 +7,7 @@ import itemsRoutes from "./routes/items.routes.js";
 dotenv.config(); // Load .env variables
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -22,6 +23,6 @@ app.get("/", (req, res) => {
   res.send("Backend is running 🚀");
 });
 
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });

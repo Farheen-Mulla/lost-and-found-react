adb125d41cd0e8c714c3b554890339cc224916fa
Farheen-Mulla <mullaf909@gmail.com>
2026-07-17 21:09:05 +0530

    update Item schema

--- lost-found-backend/src/models/Item.js
+++ lost-found-backend/src/models/Item.js
@@ -1,10 +1,25 @@
 import mongoose from "mongoose";
 const itemSchema = new mongoose.Schema({
-    name: String,
-    desc: String,
-    status: String,
-    contact: String,
-    image: String
+    name: {
+        type: String,
+        required: true,
+    },
+    desc: {
+        type: String,
+        required: true,
+    },
+    status: {
+        type: String,
+        enum: ["lost", "found"],
+        required: true,
+    },
+    contact: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+    },
 } , {timestamps: true});
 
 export default mongoose.model("Item",itemSchema);

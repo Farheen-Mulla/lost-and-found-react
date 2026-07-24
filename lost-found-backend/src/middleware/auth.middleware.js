a37783fa13b409ee184bc5732b605f47e8998969
Farheen-Mulla <mullaf909@gmail.com>
2026-07-24 10:18:45 +0530

    fix middleware

--- lost-found-backend/src/middleware/auth.middleware.js
+++ lost-found-backend/src/middleware/auth.middleware.js
@@ -1,10 +1,11 @@
 import jwt from "jsonwebtoken";
 
 export const protect = (req, res, next) => {
-  const token = req.headers.authorization;
-  if (!token) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
     return res.status(401).json({ message: "Access denied. No token provided." });
   }
+  const token = authHeader.split(" ")[1];
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
